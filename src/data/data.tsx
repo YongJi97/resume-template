bb10dd46380db8a9a2f1fc696e83e35fde2f099e
yongji <yongji.chen@openfin.co>
2025-02-13 20:27:15 -0500

    update footer

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -402,22 +402,12 @@ I am confident that Yongji would be an asset to any team. He is a hard worker, a
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Please shoot me an email if you have any questions',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
-    },
-    {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: 'yongjichen97@gmail.com',
+      href: 'mailto:yongjichen97@gmail.com',
     },
     {
       type: ContactType.Github,
